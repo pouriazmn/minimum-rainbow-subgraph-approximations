2ed7090d1acc595364ebfb2579e7639100d40b45
Pouria Zamani <pooriazmn@gmail.com>
2022-05-01 16:34:45 -0500

    A few changes in graph class and rewire

--- Graph.py
+++ Graph.py
@@ -1,74 +1,137 @@
 import random
+from collections import defaultdict
+from typing import List, Dict
+
 import networkx as nx
 from matplotlib import pyplot as plt
 
+
 class Vertex:
-    #create a vertex
-    def __init__(self):
-        self.incidentEdges = []
+    # create a vertex
+    def __init__(self, index):
+        self.index = index
+        self._incidentEdges = set()
+        self.neighbours = set()
 
-    #check how many edges this vertex is incident to
+    # check how many edges this vertex is incident to
     def degree(self):
-        return len(self.incidentEdges)
+        return len(self._incidentEdges)
 
-    #add a new edge we are incident to
+    # add a new edge we are incident to
     def addEdge(self, newEdge):
-        assert(newEdge.v1 == self or newEdge.v2 == self)
-        self.incidentEdges.append(newEdge)
+        assert (newEdge.v1 is self or newEdge.v2 is self)
+        self._incidentEdges.add(newEdge)
+        if newEdge.v1 is self:
+            self.neighbours.add(newEdge.v2)
+        else:
+            self.neighbours.add(newEdge.v1)
+
+    def removeEdge(self, edge):
+        assert (edge.v1 is self or edge.v2 is self)
+        self._incidentEdges.remove(edge)
+        if edge.v1 is self:
+            self.neighbours.remove(edge.v2)
+        else:
+            self.neighbours.remove(edge.v1)
+
+    def __str__(self):
+        return f"<{self.index}>"
+
 
 class Edge:
-    #create an edge between v1 and v2 with a specific colour
-    def __init__(self, v1, v2, colour):
+
+    # create an edge between v1 and v2 with a specific colour
+    def __init__(self, v1: Vertex, v2: Vertex, colour):
         self.v1 = v1
         self.v2 = v2
         self.colour = colour
 
+    def __eq__(self, other):
+        return (self.v1 == other.v1 and self.v2 == other.v2) or (self.v1 == other.v2 and self.v2 == other.v1)
+
+    def __ne__(self, other):
+        return not (self.__eq__(other))
+
+    def __hash__(self):
+        return id(self)
+
+    def __bool__(self):
+        return True
+
+    def __str__(self):
+        return f"<{self.v1}, {self.v2}, {self.colour}>"
+
+    def __repr__(self):
+        return f"<{self.v1}, {self.v2}, {self.colour}>"
+
+
 class Graph:
-    #create an empty graph
-    def __init__(self, maxColour):
-        self.vertices = []
-        self.edges = []
+
+    # create an empty graph
+    def __init__(self, maxColour=None):
+        self._vertex_index = 0
+        self.vertices: Dict[int][Vertex] = dict()
+        self.edges: List[Edge] = []
+        self.colours = defaultdict(list)
         self.maxColour = maxColour
 
-    #get the number of vertices in a graph
+    @property
+    def num_colours(self):
+        return len(self.colours.keys())
+
+    # get the number of vertices in the graph
     def n(self):
         return len(self.vertices)
 
+    # get the number of edges in the graph
     def m(self):
         return len(self.edges)
 
-    #add a new vertex to the graph
-    def newVertex(self):
-        self.vertices.append(Vertex())
+    @property
+    def vertex_index(self):
+        while self._vertex_index in self.vertices.keys():
+            self._vertex_index += 1
+        return self._vertex_index
 
-    #add an existing vertex to the graph
+    # add a new vertex to the graph
+    def newVertex(self, index=None):
+        if index is None:
+            index = self.vertex_index
+        if index in self.vertices.keys():
+            raise ValueError(f"Index {index} already exists in the graph")
+        v = Vertex(index)
+        self.vertices[v.index] = v
+        return v
+
+    # add an existing vertex to the graph
     def addVertex(self, v):
-        self.vertices.append(v)
+        if not v.index:
+            v.index = self.vertex_index
+        if v.index in self.vertices.keys():
+            raise ValueError(f"Vertex {v} already exists in the graph")
+        self.vertices[v.index] = v
 
-    #connect vertices u and v with an edge of a specified colour
-    def addEdge(self, u, v, colour):
-        assert(u in self.vertices and v in self.vertices)
-        # assert(colour < self.maxColour)
+    # connect vertices u and v with an edge of a specified colour
+    def addEdge(self, u: Vertex, v: Vertex, colour):
+        assert self.vertices[v.index] is v and self.vertices[u.index] is u
+        assert u is not v
         newEdge = Edge(u, v, colour)
         self.edges.append(newEdge)
         u.addEdge(newEdge)
         v.addEdge(newEdge)
+        self.colours[colour].append(newEdge)
 
-        if colour > self.maxColour:
-            self.maxColour = colour
-
-    def removeEdge(self, edge):
+    def removeEdge(self, edge: Edge):
+        assert edge is not None
         self.edges.remove(edge)
-        edge.v1.incidentEdges.remove(edge)
-        edge.v2.incidentEdges.remove(edge)
+        edge.v1.removeEdge(edge)
+        edge.v2.removeEdge(edge)
+        self.colours[edge.colour].remove(edge)
 
-    #check if vertices u and v are connected by at least one edge
-    def adjacent(self, u, v):
-        assert(u in self.vertices and v in self.vertices)
-        for edge in self.edges:
-            if (edge.v1 == u and edge.v2 == v) or (edge.v1 == v and edge.v2 == u):
-                return True
-        return False
+    # check if vertices u and v are connected by at least one edge
+    def adjacent(self, u: Vertex, v: Vertex):
+        assert self.vertices[v.index] is v and self.vertices[u.index] is u
+        return u in v.neighbours
 
     def toNX(self):
         nx_graph = nx.MultiGraph()
@@ -85,68 +148,42 @@ class Graph:
         plt.show()
 
     @classmethod
-    def from_adjacency_matrix(cls, adjacency_matrix, maxColor):
-        graph = cls(maxColor)
+    def from_adjacency_matrix(cls, adjacency_matrix):
+        graph = cls()
+
         n = len(adjacency_matrix)
-        vertices = []
         for i in range(n):
-            v = Vertex()
-            vertices.append(v)
-            graph.addVertex(v)
+            graph.newVertex(i)
         for i in range(n):
             for j in range(n):
                 if adjacency_matrix[i][j] != 0:
                     color = adjacency_matrix[i][j]
-                    if color == maxColor:
-                        color = 0
-                    graph.addEdge(vertices[i], vertices[j], color)
+                    graph.addEdge(graph.vertices[i], graph.vertices[j], color)
         return graph
 
-    def to_adjacency_matrix(self):
-        n = self.n()
-        max_colour = self.maxColour
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
-        for edge in self.edges:
-            v1 = edge.v1
-            v2 = edge.v2
-            v1_ind = self.vertices.index(v1)
-            v2_ind = self.vertices.index(v2)
-            if edge.colour == 0:
-                c = max_colour
-            else:
-                c = edge.colour
-            matrix[v1_ind][v2_ind] = c
-            matrix[v2_ind][v1_ind] = c
-
-        return matrix
-
-
     def copy(self):
-        newGraph = Graph(maxColour=self.maxColour)
-        vertices_map = {}
-        for vertex in self.vertices:
-            v = Vertex()
-            vertices_map[vertex] = v
-            newGraph.addVertex(v)
+        newGraph = Graph()
+        for index in self.vertices.keys():
+            newGraph.newVertex(index)
 
         for edge in self.edges:
-            v1 = vertices_map[edge.v1]
-            v2 = vertices_map[edge.v2]
+            v1 = newGraph.vertices[edge.v1.index]
+            v2 = newGraph.vertices[edge.v2.index]
             newGraph.addEdge(v1, v2, edge.colour)
 
         return newGraph
 
 
 def rewire(G):
-    newGraph = G.copy()
+    newGraph = G
     m = newGraph.m()
 
-    #find the first edge
-    edgeNum = random.randint(0,m-1)
+    # find the first edge
+    edgeNum = random.randint(0, m - 1)
     edge = newGraph.edges[edgeNum]
 
-    #find the first two vertices
-    vertexNum = random.randint(0,1)
+    # find the first two vertices
+    vertexNum = random.randint(0, 1)
     if vertexNum == 0:
         u1 = edge.v1
         v = edge.v2
@@ -156,31 +193,46 @@ def rewire(G):
 
     vertexDegree = u1.degree()
     u2 = None
-
-    #find the other edge and the associated vertices
+    w = None
+    otherEdge = None
+    # find the other edge and the associated vertices
     for e in newGraph.edges:
-        if e.v1 != edge.v1 and e.v2 != edge.v2 and e.v2 != edge.v1 and e.v1 != edge.v2:
-            if e.v1.degree() == vertexDegree:
+        if e != edge:
+            if e.v1.degree() == vertexDegree \
+                    and v not in e.v1.neighbours \
+                    and u1 not in e.v2.neighbours \
+                    and u1 != e.v2 \
+                    and v != e.v1:
                 u2 = e.v1
                 w = e.v2
                 otherEdge = e
-            elif e.v2.degree() == vertexDegree:
+            elif e.v2.degree() == vertexDegree \
+                    and v not in e.v2.neighbours \
+                    and u1 not in e.v1.neighbours \
+                    and u1 != e.v1 \
+                    and v != e.v2:
                 u2 = e.v2
                 w = e.v1
                 otherEdge = e
 
-        if not u2 is None:
+        if otherEdge:
             break
 
-    #if we couldn't find an edge satisfying the conditions, pick an arbitrary edge
-    if u2 is None:
-        otherEdge = edge
-        otherEdgeNum = 0
-        while otherEdge == edge:
-            otherEdge = newGraph.edges[otherEdgeNum]
-            otherEdgeNum = (otherEdgeNum + 1) % m
-        u2 = otherEdge.v1
-        w = otherEdge.v2
+    # if we couldn't find an edge satisfying the conditions, pick an arbitrary edge
+    if otherEdge is None:
+        for e in newGraph.edges:
+            if e != edge \
+                    and v not in e.v1.neighbours \
+                    and u1 not in e.v2.neighbours \
+                    and u1 != e.v2 \
+                    and v != e.v1:
+                u2 = e.v1
+                w = e.v2
+                otherEdge = e
+
+    # if still we could not find a pair of disjoint vertices to match this edge we should select the random edge again.
+    if otherEdge is None:
+        return False, newGraph
 
     newGraph.removeEdge(edge)
     newGraph.removeEdge(otherEdge)
@@ -188,12 +240,18 @@ def rewire(G):
     newGraph.addEdge(u1, w, edge.colour)
     newGraph.addEdge(u2, v, otherEdge.colour)
 
-    return newGraph
+    return True, newGraph
+
 
 def randomGraph(G):
     k = G.m()
     newGraph = G.copy()
 
     for _ in range(k):
-        newGraph = rewire(newGraph)
+        c = 2
+        success, newGraph = rewire(newGraph)
+        while not success:
+            print(f"attempt {c}")
+            success, newGraph = rewire(newGraph)
+            c += 1
     return newGraph
